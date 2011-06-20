90e9a3b34848600640956dbcb9d38b7634f4e6e4
Chuck Burgess <cdburgess@gmail.com>
2011-06-19 18:22:10 -0600

    Updating schema to reflect what the code is looking for.

--- config/schema/users_schema.sql
+++ config/schema/users_schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 05, 2010 at 04:17 PM
+-- Generation Time: Jun 19, 2011 at 06:15 PM
 -- Server version: 5.1.44
 -- PHP Version: 5.2.13
 
@@ -14,71 +14,34 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
-
-CREATE TABLE `groups` (
-  `id` char(36) NOT NULL,
-  `name` varchar(40) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` VALUES('4c5b190d-4b7c-46a8-b163-3006e4ca782d', 'Admin', '2010-08-05 14:03:25', '2010-08-05 14:03:25');
-INSERT INTO `groups` VALUES('4c5b1bf4-0148-4e2c-ae63-308de4ca782d', 'User', '2010-08-05 14:15:48', '2010-08-05 14:15:48');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups_permissions`
---
-
-CREATE TABLE `groups_permissions` (
-  `group_id` char(36) NOT NULL,
-  `permission_id` char(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Table structure for table `groups_users`
---
-
-CREATE TABLE `groups_users` (
-  `group_id` char(36) NOT NULL,
-  `user_id` char(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 --
 -- Table structure for table `permissions`
 --
 
 CREATE TABLE `permissions` (
   `id` char(36) NOT NULL,
-  `name` varchar(40) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `role` enum('Admin','User','Manager','Guest') NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `role` (`role`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` char(36) NOT NULL,
-  `email_address` varchar(127) NOT NULL,
-  `password` varchar(40) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '0',
+  `email_address` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `facebook_id` int(11) DEFAULT NULL,
+  `role` enum('Admin','User','Manager','Guest') NOT NULL DEFAULT 'User',
+  `active` enum('1','0') NOT NULL DEFAULT '0',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`email_address`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
