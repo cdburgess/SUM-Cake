d4f1811506ccf3e051955dd574cbfca32adfb459
Chuck <cdburgess@gmail.com>
2012-09-01 19:09:45 -0600

    Built a schema migration to include the details for the Google Authenticator.

--- Config/Schema/schema.sql
+++ Config/Schema/schema.sql
@@ -5,32 +5,44 @@ DROP TABLE IF EXISTS `users`;
 
 
 CREATE TABLE `permissions` (
-	`id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`name` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`role` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`copied_from` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '',
-	`created` datetime NOT NULL COMMENT '',
-	`modified` datetime NOT NULL COMMENT '',	PRIMARY KEY  (`id`),
-	KEY `role` (`role`),
-	KEY `copied_from` (`copied_from`))	DEFAULT CHARSET=utf8,
-	COLLATE=utf8_general_ci,
-	ENGINE=MyISAM;
+  `id` char(36) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  `copied_from` varchar(10) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`,`role`),
+  KEY `copied_from` (`copied_from`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `roles` (
+  `id` char(36) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE `users` (
-	`id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`email_address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`first_name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`last_name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '',
-	`role` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT 'User' NOT NULL COMMENT '',
-	`active` tinyint(1) DEFAULT '0' NOT NULL COMMENT '',
-	`disabled` tinyint(1) DEFAULT '0' NOT NULL COMMENT '',
-	`password_requested` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '0' NOT NULL COMMENT '',
-	`created` datetime NOT NULL COMMENT '',
-	`modified` datetime NOT NULL COMMENT '',	PRIMARY KEY  (`id`),
-	UNIQUE KEY `username` (`email_address`),
-	KEY `disabled` (`disabled`),
-	KEY `active` (`active`),
-	KEY `password_requested` (`password_requested`))	DEFAULT CHARSET=utf8,
-	COLLATE=utf8_general_ci,
-	ENGINE=MyISAM;
+  `id` varchar(36) NOT NULL,
+  `username` varchar(48) NOT NULL,
+  `email_address` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'User',
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `disabled` tinyint(1) NOT NULL DEFAULT '0',
+  `token_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `authenticator_key` varchar(16) DEFAULT NULL,
+  `password_requested` varchar(36) NOT NULL DEFAULT '0',
+  `last_login` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `emial` (`email_address`),
+  UNIQUE KEY `username` (`username`),
+  KEY `disabled` (`disabled`),
+  KEY `active` (`active`),
+  KEY `password_requested` (`password_requested`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- Config/Schema/user_sample_data.sql
+++ Config/Schema/user_sample_data.sql
@@ -5,7 +5,12 @@ INSERT INTO `permissions` (`id`, `name`, `role`, `copied_from`, `created`, `modi
 INSERT INTO `permissions` (`id`, `name`, `role`, `copied_from`, `created`, `modified`) VALUES('4c674534-e200-4b6b-843f-119de4ca782d', 'Users:view', 'User', NULL, '2010-08-14 19:39:00', '2010-08-14 19:39:00');
 INSERT INTO `permissions` (`id`, `name`, `role`, `copied_from`, `created`, `modified`) VALUES('4c67453b-684c-4530-b5b8-119de4ca782d', 'Users:edit', 'User', NULL, '2010-08-14 19:39:07', '2010-08-14 19:39:07');
 INSERT INTO `permissions` (`id`, `name`, `role`, `copied_from`, `created`, `modified`) VALUES('4c6751af-d284-4449-8f8b-12f8e4ca782d', 'Users:change_password', 'User', NULL, '2010-08-14 20:32:15', '2010-08-14 20:32:15');
+INSERT INTO `permissions` (`id`, `name`, `role`, `copied_from`, `created`, `modified`) VALUES('50209930-6f88-4043-b1ce-45ffe4ca782d', 'Users:enable_token', 'User', NULL, '2012-08-07 04:27:28', '2012-08-07 04:27:28');
+
+INSERT INTO `roles` (`id`, `name`) VALUES('4fd149e5-022c-46cc-8cdb-1381e4ca782d', 'Admin');
+INSERT INTO `roles` (`id`, `name`) VALUES('4fd149ef-214c-44e7-9793-0d58e4ca782d', 'User');
+INSERT INTO `roles` (`id`, `name`) VALUES('4fdcc3df-b460-4f29-912e-56c3e4ca782d', 'Manager');
 
 
-INSERT INTO `users` (`id`, `email_address`, `password`, `first_name`, `last_name`, `role`, `active`, `disabled`, `password_requested`, `created`, `modified`) VALUES('4c5b1927-5e14-4379-95d1-3009e4ca782d', 'admin@example.com', '5cca727f3f7ee0c868a1980cdf9252415fd7a1d1', 'Admin', 'User', 'Admin', '1', '0', '0', '2010-08-05 14:03:51', '2012-01-15 00:56:08');
-INSERT INTO `users` (`id`, `email_address`, `password`, `first_name`, `last_name`, `role`, `active`, `disabled`, `password_requested`, `created`, `modified`) VALUES('4dfec325-c338-49e5-983c-43a8e4ca782d', 'user@example.com', '5cca727f3f7ee0c868a1980cdf9252415fd7a1d1', 'Example', 'User', 'User', '1', '0', '0', '2011-06-19 21:48:53', '2012-01-15 00:27:44');
+INSERT INTO `users` (`id`, `username`, `email_address`, `password`, `first_name`, `last_name`, `role`, `active`, `disabled`, `token_enabled`, `authenticator_key`, `password_requested`, `last_login`, `created`, `modified`) VALUES('4c5b1927-5e14-4379-95d1-3009e4ca782d', 'admin', 'admin@example.com', '5cca727f3f7ee0c868a1980cdf9252415fd7a1d1', 'Admin', 'User', 'Admin', 1, 0, 0, NULL, '0', '2012-08-07 04:27:10', '2010-08-05 14:03:51', '2012-08-07 04:27:10');
+INSERT INTO `users` (`id`, `username`, `email_address`, `password`, `first_name`, `last_name`, `role`, `active`, `disabled`, `token_enabled`, `authenticator_key`, `password_requested`, `last_login`, `created`, `modified`) VALUES('4dfec325-c338-49e5-983c-43a8e4ca782d', 'user', 'user@example.com', '5cca727f3f7ee0c868a1980cdf9252415fd7a1d1', 'Example', 'User', 'User', 1, 0, 0, NULL, '0', '2012-08-28 04:13:26', '2011-06-19 21:48:53', '2012-08-28 04:14:02');
